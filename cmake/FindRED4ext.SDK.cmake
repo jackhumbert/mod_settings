b3db9c7a45ca511845f2102b26ec165498b07d0e
Jack Humbert <jack.humb@gmail.com>
2023-04-23 13:39:43 -0400

    start cmake conversion

---
+++ cmake/FindRED4ext.SDK.cmake
@@ -0,0 +1,11 @@
+option(RED4EXT_USE_PCH "" ON)
+option(RED4EXT_HEADER_ONLY "" ON)
+
+add_subdirectory(${PROJECT_SOURCE_DIR}/deps/red4ext.sdk)
+set_target_properties(RED4ext.SDK PROPERTIES FOLDER "Dependencies")
+
+mark_as_advanced(
+  RED4EXT_BUILD_EXAMPLES
+  RED4EXT_USE_PCH
+  RED4EXT_INSTALL
+)
