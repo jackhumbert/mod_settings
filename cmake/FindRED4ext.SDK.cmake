4bac18018356d25278f02ee6f651b7e03951b221
Jack Humbert <jack.humb@gmail.com>
2023-04-26 14:55:34 -0400

    safely load red4ext

--- cmake/FindRED4ext.SDK.cmake
+++ cmake/FindRED4ext.SDK.cmake
@@ -1,13 +1,15 @@
-option(RED4EXT_USE_PCH "" ON)
-option(RED4EXT_HEADER_ONLY "" ON)
+if(NOT DEFINED MOD_RED4EXT_SDK_DIR)
+  option(RED4EXT_USE_PCH "" ON)
+  option(RED4EXT_HEADER_ONLY "" ON)
 
-add_subdirectory(${PROJECT_SOURCE_DIR}/deps/red4ext.sdk)
-set_target_properties(RED4ext.SDK PROPERTIES FOLDER "Dependencies")
+  add_subdirectory(deps/red4ext.sdk)
+  set_target_properties(RED4ext.SDK PROPERTIES FOLDER "Dependencies")
 
-mark_as_advanced(
-  RED4EXT_BUILD_EXAMPLES
-  RED4EXT_USE_PCH
-  RED4EXT_INSTALL
-)
+  mark_as_advanced(
+    RED4EXT_BUILD_EXAMPLES
+    RED4EXT_USE_PCH
+    RED4EXT_INSTALL
+  )
 
-set(MOD_RED4EXT_SDK_DIR ${PROJECT_SOURCE_DIR}/deps/red4ext.sdk)
+  set(MOD_RED4EXT_SDK_DIR "${MOD_SOURCE_DIR}/deps/red4ext.sdk")
+endif()
