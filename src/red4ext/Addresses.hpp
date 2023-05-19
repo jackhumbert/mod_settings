cb6ead5f94bdebe6ae4691fa06deddb5fc267048
Jack Humbert <jack.humb@gmail.com>
2023-05-19 10:26:09 -0400

    update for cmake

--- src/red4ext/Addresses.hpp
+++ src/red4ext/Addresses.hpp
@@ -4,57 +4,6 @@
 #define ZOLTAN_EXPAND_THEN_STRINGISE(N) ZOLTAN_STRINGISE(N)
 #define __LINE_STR__ ZOLTAN_EXPAND_THEN_STRINGISE(__LINE__)
 
-#ifndef BaseStream_ReadWriteHandleID_Addr
-#define BaseStream_ReadWriteHandleID_Addr 0x720670
-#endif
-#ifndef CBaseRTTIType_VFT_Addr
-#define CBaseRTTIType_VFT_Addr 0x311F4D0
-#endif
-#ifndef CClass_AddEventCallbackType_Addr
-#define CClass_AddEventCallbackType_Addr 0x2034E0
-#endif
-#ifndef CClass_ApplyFlags_Addr
-#define CClass_ApplyFlags_Addr 0x206EF0
-#endif
-#ifndef CClass_SetupScriptCallbacks_Addr
-#define CClass_SetupScriptCallbacks_Addr 0x2035C0
-#endif
-#ifndef CClass_VFT_Addr
-#define CClass_VFT_Addr 0x311F2C0
-#endif
-#ifndef CGameFramework_Systems_120_128_130_Addr
-#define CGameFramework_Systems_120_128_130_Addr 0x13B3860
-#endif
-#ifndef IGameInstance_RegisterGameLoadCallbacks_Addr
-#define IGameInstance_RegisterGameLoadCallbacks_Addr 0x2D9AFD0
-#endif
-#ifndef IGameInstance_RegisterStreamingWorldLoadedCallback_Addr
-#define IGameInstance_RegisterStreamingWorldLoadedCallback_Addr 0x2D9B4F0
-#endif
-#ifndef IGameInstance_Systems158_Addr
-#define IGameInstance_Systems158_Addr 0x2D9B460
-#endif
-#ifndef IGameInstance_Systems160_Addr
-#define IGameInstance_Systems160_Addr 0x2D9B420
-#endif
-#ifndef IGameInstance_Systems168o170_Addr
-#define IGameInstance_Systems168o170_Addr 0x2D9BAF0
-#endif
-#ifndef IGameInstance_Systems178_Addr
-#define IGameInstance_Systems178_Addr 0x2D9AE20
-#endif
-#ifndef IGameInstance_WorldDetached_Addr
-#define IGameInstance_WorldDetached_Addr 0x2D9AD90
-#endif
-#ifndef IGameInstance_WorldPendingDetach_Addr
-#define IGameInstance_WorldPendingDetach_Addr 0x2D9B790
-#endif
-#ifndef ISerializable_SetOwner_Addr
-#define ISerializable_SetOwner_Addr 0x1BA2A0
-#endif
-#ifndef ISerializable_VFT_Addr
-#define ISerializable_VFT_Addr 0x31179A8
-#endif
 #ifndef ReleaseScriptData_Addr
 #define ReleaseScriptData_Addr 0x27E1B0
 #endif
