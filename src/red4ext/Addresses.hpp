f8f52fca2507cdc0377d34de55c2b2e6ac479ff3
Jack Humbert <jack.humb@gmail.com>
2023-10-07 18:26:27 -0400

    update

--- src/red4ext/Addresses.hpp
+++ src/red4ext/Addresses.hpp
@@ -5,5 +5,5 @@
 #define __LINE_STR__ ZOLTAN_EXPAND_THEN_STRINGISE(__LINE__)
 
 #ifndef ReleaseScriptData_Addr
-#define ReleaseScriptData_Addr 0x6EBFAC
+#define ReleaseScriptData_Addr 0x7128EC
 #endif
--- src/red4ext/Hooks/ReleaseScriptData.cpp
+++ src/red4ext/Hooks/ReleaseScriptData.cpp
@@ -3,7 +3,7 @@
 #include "ModSettings.hpp"
 #include <Registrar.hpp>
 
-/// @pattern 40 53 48 83 EC 20 48 8B D9 E8 92 00 00 00 48 8D 8B 38 01 00 00 FF 15 71 D5 2B 02 48 8D 8B 00 01
+/// @pattern 40 53 48 83 EC 20 48 8B D9 E8 92 00 00 00 48 8D 8B 38 01 00 00 FF 15 ? ? ? 02 48 8D 8B 00 01
 void *__fastcall ReleaseScriptData(ModSettings::ScriptData *scriptData);
 
 REGISTER_HOOK(void *__fastcall, ReleaseScriptData, ModSettings::ScriptData *scriptData) {
