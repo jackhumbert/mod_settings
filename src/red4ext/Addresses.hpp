1f7eceec7b7fcee5c340bd91457b9931e807a652
Jack Humbert <jack.humb@gmail.com>
2024-01-31 18:45:28 -0500

    update for 2.11

--- src/red4ext/Addresses.hpp
+++ src/red4ext/Addresses.hpp
@@ -5,5 +5,5 @@
 #define __LINE_STR__ ZOLTAN_EXPAND_THEN_STRINGISE(__LINE__)
 
 #ifndef ReleaseScriptData_Addr
-#define ReleaseScriptData_Addr 0x956840
+#define ReleaseScriptData_Addr 0x1005F4
 #endif
