f0b055cba881ab8fc44324ef3e729b2db5c8ea7d
Jack Humbert <jack.humb@gmail.com>
2024-03-01 10:00:42 -0500

    updat for 2.12

--- src/red4ext/Addresses.hpp
+++ src/red4ext/Addresses.hpp
@@ -4,6 +4,3 @@
 #define ZOLTAN_EXPAND_THEN_STRINGISE(N) ZOLTAN_STRINGISE(N)
 #define __LINE_STR__ ZOLTAN_EXPAND_THEN_STRINGISE(__LINE__)
 
-#ifndef ReleaseScriptData_Addr
-#define ReleaseScriptData_Addr 0x1005F4
-#endif
--- src/red4ext/Hooks/ReleaseScriptData.cpp
+++ src/red4ext/Hooks/ReleaseScriptData.cpp
@@ -3,10 +3,11 @@
 #include "ModSettings.hpp"
 #include <Registrar.hpp>
 
-/// @hash 3993832650
-void *__fastcall ReleaseScriptData(ModSettings::ScriptData *scriptData);
+// @hash 3993832650
+// void *__fastcall ReleaseScriptData(ModSettings::ScriptData *scriptData);
+// RED4ext::UniversalRelocFunc<void (*)(ModSettings::ScriptData *)> ReleaseScriptData(3993832650);
 
-REGISTER_HOOK(void *__fastcall, ReleaseScriptData, ModSettings::ScriptData *scriptData) {
+REGISTER_HOOK_HASH(void *, 3993832650, ReleaseScriptData, ModSettings::ScriptData *scriptData) {
   ModSettings::ModSettings::ProcessScriptData(scriptData);
   auto og = ReleaseScriptData_Original(scriptData);
   return og;
