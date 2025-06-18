207df8731e9f31d7ad9549bcd66052fae3a6af05
Jack Humbert <jack.humb@gmail.com>
2025-06-17 22:35:01 -0400

    use hashes for fund types

--- src/red4ext/Utils.hpp
+++ src/red4ext/Utils.hpp
@@ -20,77 +20,77 @@ template<> void RedTypeFromString<bool>(bool * pointer, const RED4ext::CString& 
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Bool)->FromString(pointer, buffer);
+  RTTITYPE(3443526637)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<int8_t>(int8_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Int8)->FromString(pointer, buffer);
+  RTTITYPE(3415542724)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<int16_t>(int16_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Int16)->FromString(pointer, buffer);
+  RTTITYPE(3592162291)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<int32_t>(int32_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Int32)->FromString(pointer, buffer);
+  RTTITYPE(3590851569)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<int64_t>(int64_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Int64)->FromString(pointer, buffer);
+  RTTITYPE(3594652662)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<uint8_t>(uint8_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Uint8)->FromString(pointer, buffer);
+  RTTITYPE(3648392249)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<uint16_t>(uint16_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Uint16)->FromString(pointer, buffer);
+  RTTITYPE(3832679528)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<uint32_t>(uint32_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Uint32)->FromString(pointer, buffer);
+  RTTITYPE(3831368806)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<uint64_t>(uint64_t * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Uint64)->FromString(pointer, buffer);
+  RTTITYPE(3835169899)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<float>(float * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Float)->FromString(pointer, buffer);
+  RTTITYPE(3665562711)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<double>(double * pointer, const RED4ext::CString& str) {
   RED4ext::RawBuffer buffer;
   buffer.data = (void*)str.c_str();
   buffer.size = str.length;
-  RTTITYPE(Double)->FromString(pointer, buffer);
+  RTTITYPE(3892120764)->FromString(pointer, buffer);
 }
 
 template<> void RedTypeFromString<RED4ext::CName>(RED4ext::CName * pointer, const RED4ext::CString& str) {
